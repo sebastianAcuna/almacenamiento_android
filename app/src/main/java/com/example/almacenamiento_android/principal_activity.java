0f97d65c4210d0ef6887cad118e407ae9dd118b6
unknown <seba.nobody@gmail.com>
2019-11-13 23:55:31 -0300

    first

---
+++ app/src/main/java/com/example/almacenamiento_android/principal_activity.java
@@ -0,0 +1,16 @@
+package com.example.almacenamiento_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class principal_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_activity);
+    }
+
+
+}
